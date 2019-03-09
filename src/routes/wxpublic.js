d954c9cafc7e99a8d19ee1ff68bf2da462a4837e
shadowprompt <huixiong.cn@gmail.com>
2019-03-09 11:16:16 +0800

    增加微信小程序的服务器校验

---
+++ src/routes/wxpublic.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+// 供微信服务器测试接口地址合法
+router.get('/', (req, res) => {
+  const {
+    echostr
+  } = req.query;
+  res.send(echostr);
+});
+module.exports = router;
